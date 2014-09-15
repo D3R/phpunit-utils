fb94db2426e05cdbc412feaff5bbbf4d379004a5
Ronan <ronan@d3r.com>
2014-09-15 13:45:15 +0100

    Initial implementation of Selenium2 TestCase Add exception class

---
+++ src/phpunit/Selenium2/InvalidSetupException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace D3R\PHPUnit\Selenium2;
+
+/**
+ * Exception to be thrown for setup errors
+ *
+ * @author    Ronan Chilvers <ronan@d3r.com>
+ * @copyright 2014 D3R Ltd
+ * @license   http://d3r.com/license D3R Software Licence
+ * @package   D3R\PHPUnit\Selenium2
+ */
+class InvalidSetupException extends Exception
+{
+}
--- src/phpunit/Selenium2/TestCase.php
+++ src/phpunit/Selenium2/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace D3R\PHPUnit\Selenium2;
 
+use D3R\PHPUnit\Selenium2\InvalidSetupException;
+
 /**
  * Base TestCase for Selenium2 tests
  *
@@ -10,7 +12,34 @@ namespace D3R\PHPUnit\Selenium2;
  * @license   http://d3r.com/license D3R Software Licence
  * @package   D3R\PHPUnit\Selenium2
  */
-class TestCase extends PHPUnit_Extensions_Selenium2TestCase
+abstract class TestCase extends \PHPUnit_Extensions_Selenium2TestCase
 {
+    /**
+     * The browser selection for this testcase
+     *
+     * @var boolean|string
+     */
+    protected $browser = false;
 
+    /**
+     * The initial browser url before testing
+     *
+     * @var string
+     */
+    protected $browserUrl = '';
+
+    /**
+     * {@inheritdoc}. For Selenium 2 testing we set the browser
+     * and initial url.
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    protected function setUp()
+    {
+        if (false === $this->browser) {
+            throw new InvalidSetupException('Browser selection not set');
+        }
+        $this->setBrowser($this->browser);
+        $this->setBrowserUrl($this->browserUrl);
+    }
 }
