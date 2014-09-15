0c13cca51646ec11d7e2aea81a608acd7d16f315
Ronan <ronan@d3r.com>
2014-09-15 13:29:49 +0100

    add composer.json, composer update and add base testcase

---
+++ src/phpunit/Selenium2/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace D3R\PHPUnit\Selenium2;
+
+/**
+ * Base TestCase for Selenium2 tests
+ *
+ * @author    Ronan Chilvers <ronan@d3r.com>
+ * @copyright 2014 D3R Ltd
+ * @license   http://d3r.com/license D3R Software Licence
+ * @package   D3R\PHPUnit\Selenium2
+ */
+class TestCase extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+}
