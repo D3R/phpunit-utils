9761760e46d03997bfe063d3f4453977b31e7598
Ronan <ronan@d3r.com>
2014-09-24 12:16:35 +0100

    Implement basic fixture handling in base db testcase

--- src/phpunit/Database/TestCase.php
+++ src/phpunit/Database/TestCase.php
@@ -27,6 +27,42 @@ abstract class TestCase extends \PHPUnit_Extensions_Database_TestCase
     private $connection = null;
 
     /**
+     * The base path for fixture data files
+     *
+     * @var array
+     */
+    protected $fixturePath = false;
+
+    /**
+     * The names of the fixtures to load for this test
+     *
+     * @var array
+     */
+    protected $fixtures = array();
+
+    /**
+     * Class constructor
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->setUpFixtures();
+    }
+
+    /**
+     * Setup fixture information
+     *
+     * @return void
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    protected function setUpFixtures()
+    {
+    }
+
+    /**
      * Get the db connection for this test case
      *
      * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
@@ -42,5 +78,57 @@ abstract class TestCase extends \PHPUnit_Extensions_Database_TestCase
         }
 
         return $this->connection;
+    }
+
+    /**
+     * Override to create the test dataset
+     *
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function getDataSet()
+    {
+        if (false === $this->fixturePath) {
+            throw new \RuntimeException(
+                'Fixture path not provided in TestCase class'
+            );
+        }
+
+        $composite = new \PHPUnit_Extensions_Database_DataSet_CompositeDataSet(
+            array()
+        );
+
+        $fixturePath = rtrim($this->fixturePath, DIRECTORY_SEPARATOR);
+        foreach ($this->fixtures as $fixture) {
+            $path = implode(DIRECTORY_SEPARATOR, array($fixturePath, $fixture));
+
+            $extension = substr($path, -3);
+            switch (strtolower($extension)) {
+
+                case 'yml':
+                    $dataset = $this->createYamlDataSet($path);
+                    break;
+
+                default:
+                    throw new \RuntimeException(
+                        'Unknown data set type ' . $fixture
+                    );
+
+            }
+            $composite->addDataSet($dataset);
+        }
+
+        return $composite;
+    }
+
+    /**
+     * Get a dataset from a YAML fixture file
+     *
+     * @return PHPUnit_Extensions_Database_DataSet_YamlDataSet
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    protected function createYamlDataSet($filename)
+    {
+        return new \PHPUnit_Extensions_Database_DataSet_YamlDataSet($filename);
     }
 }
